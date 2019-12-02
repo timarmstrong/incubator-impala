vtstran/impala

IMPALA-9182: Print the socket address of the client closing a session or cancelling a query from the WebUI
This change appends the socket address (HOST:PORT) of the client
who made the request to close a session or cancel a query from
the coordinator's debug WebUI.

Existing statuses:
"Cancelled from Impala's debug web interface"
"Session closed from Impala's debug web interface"

New statuses:
"Cancelled from Impala's debug web interface by client at
 <host>:<port>"
"Session closed from Impala's debug web interface by client
 at <host>:<port>"

Testing:
-Verified visually that the status message is printed in the impalad
 log with the socket address when one cancels a query or closes a session.
-Added a new e2e test to verify that the new status gets printed in
 runtime profile and coordinator log when a query is cancelled in this
 way.

Change-Id: Icf74ad06ce1c40fab4ce37de6b7ca78e3e520b43

be/src/util/webserver.cc
@@ -578,6 +578,7 @@ sq_callback_result_t Webserver::BeginRequestCallback(struct sq_connection* conne
   }
 
   WebRequest req;
+  req.src_sock = GetRemoteAddress(request_info).ToString();
   if (request_info->query_string != nullptr) {
     req.query_string = request_info->query_string;
     BuildArgumentMap(request_info->query_string, &req.parsed_args);

timarmstrong/incubator-impala

be/src/util/webserver.cc
@@ -578,6 +578,7 @@ sq_callback_result_t Webserver::BeginRequestCallback(struct sq_connection* conne
   }
 
   WebRequest req;
+  req.source_socket = GetRemoteAddress(request_info).ToString();
   if (request_info->query_string != nullptr) {
     req.query_string = request_info->query_string;
     BuildArgumentMap(request_info->query_string, &req.parsed_args);